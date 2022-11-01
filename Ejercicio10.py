cb1ab355e4f02e011b4017e1d152d59969c73cca
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-11-01 19:24:09 +0100

    Ejercicio10

---
+++ Ejercicio10.py
@@ -0,0 +1,6 @@
+precios = [50, 75, 46, 22, 80, 65, 8]
+precio_minimo = min(precios)
+precio_maximo = max(precios)
+
+print("El mínimo de los precios es ", precio_minimo)
+print("El máximo de los precios es ", precio_maximo)
